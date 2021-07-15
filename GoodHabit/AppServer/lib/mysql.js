ab83d567480ec6af492bf32ff88ec71c7785333c
homelala <homelala@naver.com>
2021-07-15 22:07:33 +0900

    appServer 07/15

--- GoodHabit/AppServer/lib/mysql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password :'passwd',
-    database:'good'
-})
-db.connect();
-module.exports = db;
