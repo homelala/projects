bed81e42de03fdfd6d3d656f505f859f0dc80e1d
homelala <homelala@naver.com>
2021-09-23 16:45:42 +0900

    add client

--- GHAppServer/AppServer/routes/classType.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const templateRegister = require('../lib/templates/Register');
-const classTypeController = require('../controller/classTypeController');
-var router = express.Router();
-
-router.get('/register',classTypeController.TemplateRegisterClass)
-router.post('/register_process',classTypeController.createClassType);
-router.get('/list',classTypeController.classTypeList);
-module.exports = router;
